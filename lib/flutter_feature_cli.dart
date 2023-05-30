3919aba612e61b4997b10828bb3c9610e20b6cd2
Flavio Vieira <flavioemerson1@outlook.com>
2023-05-30 14:20:45 -0300

    adicionado estrutura inicial

---
+++ lib/flutter_feature_cli.dart
@@ -0,0 +1,150 @@
+import 'dart:io';
+
+import 'package:basic_utils/basic_utils.dart';
+import 'package:cli_script/cli_script.dart';
+import 'package:path/path.dart' as p;
+
+// Esse é para criar feature dentro do proprio projeto sem package de logica
+void main(List<String> args) {
+  final entityAndModelNames = <String>{};
+  final currentDir = Directory.current;
+  final finalDir = p.join(
+    currentDir.path,
+    'lib',
+    'src',
+    'features',
+  );
+  print('🚀 Type your feature name 🚀');
+  final featureNameRaw = stdin.readLineSync();
+  final featureName =
+      StringUtils.camelCaseToLowerUnderscore(featureNameRaw ?? '');
+  String exit = '';
+  print(
+    '🚨 Type all entity/model names for $featureNameRaw ⛔️without "Entity" or "Model" at the end⛔️, and in ⛔️CamelCase⛔️. (Obs: Type "e" to exit)🚨',
+  );
+  while (exit.toLowerCase() != 'e') {
+    final entitieName = stdin.readLineSync();
+    exit = entitieName ?? '';
+    if (entitieName != null && entitieName != 'e') {
+      entityAndModelNames.add(entitieName);
+      print(
+        '⚡️ Your entities/Models ${entityAndModelNames.toString()} ⚡️',
+      );
+    }
+  }
+  final pathToDatasource = '$finalDir/$featureName/data/datasource';
+  final pathToRepositoryInterface = '$finalDir/$featureName/domain/repository';
+  final pathToRepositoryImpl = '$finalDir/$featureName/data/repository';
+  final pathToUsecase = '$finalDir/$featureName/domain/usecase';
+  final pathToEntity = '$finalDir/$featureName/domain/entity';
+  final pathToModel = '$finalDir/$featureName/data/model';
+  final pathToPage = '$finalDir/$featureName/presenter/pages';
+  final pathToCubit = '$finalDir/$featureName/presenter/cubits';
+  final pathToError = '$finalDir/$featureName/domain/errors';
+  final testDir = finalDir.replaceAll('/lib', '/test');
+
+  final mkdir = Platform.isMacOS ? 'mkdir -p' : 'mkdir';
+
+  wrapMain(() async {
+    await run(
+      "$mkdir $finalDir/$featureName",
+      runInShell: true,
+    );
+    await run(
+      "$mkdir $testDir/$featureName",
+      runInShell: true,
+    );
+    await run(
+      "$mkdir $finalDir/$featureName/data",
+      runInShell: true,
+    );
+    await run(
+      "$mkdir $testDir/$featureName/data",
+      runInShell: true,
+    );
+    await run(
+      "$mkdir $finalDir/$featureName/data/datasource",
+      runInShell: true,
+    );
+    await run(
+      "$mkdir $testDir/$featureName/data/datasource",
+      runInShell: true,
+    );
+    await run(
+      "$mkdir $finalDir/$featureName/data/repository",
+      runInShell: true,
+    );
+    await run(
+      "$mkdir $testDir/$featureName/data/repository",
+      runInShell: true,
+    );
+    await run(
+      "$mkdir $finalDir/$featureName/data/model",
+      runInShell: true,
+    );
+    await run(
+      "$mkdir $testDir/$featureName/data/model",
+      runInShell: true,
+    );
+    await run(
+      "$mkdir $finalDir/$featureName/domain",
+      runInShell: true,
+    );
+    await run(
+      "$mkdir $testDir/$featureName/domain",
+      runInShell: true,
+    );
+    await run(
+      "$mkdir $finalDir/$featureName/domain/usecase",
+      runInShell: true,
+    );
+    await run(
+      "$mkdir $testDir/$featureName/domain/usecase",
+      runInShell: true,
+    );
+    await run(
+      "$mkdir $finalDir/$featureName/domain/repository",
+      runInShell: true,
+    );
+    await run(
+      "$mkdir $testDir/$featureName/domain/entity",
+      runInShell: true,
+    );
+    await run(
+      "$mkdir $testDir/$featureName/domain/errors",
+      runInShell: true,
+    );
+    await run(
+      "$mkdir $finalDir/$featureName/domain/errors",
+      runInShell: true,
+    );
+    await run(
+      "$mkdir $testDir/$featureName/presenter/pages",
+      runInShell: true,
+    );
+    await run(
+      "$mkdir $testDir/$featureName/presenter/widget",
+      runInShell: true,
+    );
+    await run(
+      "$mkdir $testDir/$featureName/presenter/cubit",
+      runInShell: true,
+    );
+    await run(
+      "$mkdir $finalDir/$featureName/presenter/pages",
+      runInShell: true,
+    );
+    await run(
+      "$mkdir $finalDir/$featureName/presenter/widgets",
+      runInShell: true,
+    );
+    await run(
+      "$mkdir $finalDir/$featureName/presenter/cubits",
+      runInShell: true,
+    );
+    await run(
+      "$mkdir $finalDir/$featureName/domain/entity",
+      runInShell: true,
+    );
+  });
+}
---
+++ lib/helper/whrite_files.dart
@@ -0,0 +1,33 @@
+import 'package:basic_utils/basic_utils.dart';
+String _capitalize(String s) => s[0].toUpperCase() + s.substring(1);
+String _invertCapitalize(String s) => s[0].toLowerCase() + s.substring(1);
+void writeFile({
+  required Set<String> data,
+  required String path,
+  required String featureName,
+  required String featureNameRaw,
+  required String finalDir,
+  String? modelPath,
+  String? repositoryImplPath,
+  String? datasourceImplPath,
+  bool writeEntity = false,
+  bool writeUsecase = false,
+  bool writeRepository = false,
+  bool writeDatasource = false,
+  bool writePage = false,
+  bool writeCubit = false,
+  bool writeError = false,
+}) {
+  final capitalizedFeatureName = _capitalize(featureNameRaw);
+  final usecaseInterfaceFileName = '${capitalizedFeatureName}Usecase';
+  final usecaseImplFileName = '${capitalizedFeatureName}UsecaseImpl';
+
+  final repositoryInterfaceFileName = '${capitalizedFeatureName}Repository';
+  final repositoryImplFileName = '${capitalizedFeatureName}RepositoryImpl';
+
+  final datasourceInterfaceFileName = '${capitalizedFeatureName}Datasource';
+  final datasourceImplFileName = '${capitalizedFeatureName}DatasourceImpl';
+  final testDir = finalDir.replaceAll('/lib', '/test');
+  final featureNameInCamelCase =
+      StringUtils.camelCaseToLowerUnderscore(featureName);
+}
